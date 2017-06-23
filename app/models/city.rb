9a6d2c644c81fd789225cb032308e72c8d301d06
github username <github email address>
2017-06-23 18:02:18 -0400

    123 main street.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,20 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    t1 = Date.parse(start_date)
+    t2 = Date.parse(end_date)
+    squirrel = Listing.all.select do |listing|
+      acorn = listing.reservations.select do |reservation|
+        reservation.checkout < t1 || reservation.checkin > t2
+        binding.pry
+      end
+      !acorn.empty?
+    end
+    squirrel << Listing.all.select do |listing|
+      listing.reservations.empty?
+    end
+    squirrel.flatten
+    binding.pry
+  end
 end
-
