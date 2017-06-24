092498a52eb2101220146d19ca19c6d471ed0574
jeremylenz <jeremylenz@gmail.com>
2017-06-24 16:22:09 -0400

    fixed first method

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,17 +5,19 @@ class City < ActiveRecord::Base
   def city_openings(start_date, end_date)
     t1 = Date.parse(start_date)
     t2 = Date.parse(end_date)
-    squirrel = Listing.all.select do |listing|
-      acorn = listing.reservations.select do |reservation|
-        reservation.checkout < t1 || reservation.checkin > t2
-        binding.pry
+    puts "Checking #{self.name} availability for #{t1} through #{t2}..."
+    squirrel = self.listings.to_a
+    squirrel.select! do |listing|
+      acorn = listing.reservations.select do |reservation|  # List all the reservations that conflict
+        reservation.conflicts?(t1, t2)
       end
-      !acorn.empty?
+      # binding.pry
+      acorn.empty?  # If the list is empty, add the listing to the array of available listings.
     end
-    squirrel << Listing.all.select do |listing|
-      listing.reservations.empty?
-    end
-    squirrel.flatten
-    binding.pry
+    squirrel.flatten!
+    puts "Returning #{squirrel.length} listings."
+    squirrel
   end
+
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  def conflicts? (desired_checkin, desired_checkout)
+    if self.checkout < desired_checkin || self.checkin > desired_checkout
+      puts "Reservation for #{self.checkin} to #{self.checkout} is ok."
+      return false
+    else
+      puts "Reservation for #{self.checkin} to #{self.checkout} conflicts."
+      return true
+    end
+  end
+
 end
